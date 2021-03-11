d53cf0ba971f3e89b899c20c6f2aa00890fdff4c
charly3pins <charlyfb90@gmail.com>
2021-03-11 18:17:24 +0100

    feat :sparkles:: theme switcher and code syntax styles

---
+++ static/js/theme-switcher.js
@@ -0,0 +1,18 @@
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    console.log("hola")
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var themeSwitcher = document.getElementById("theme-switcher");
+
+    themeSwitcher.onclick = function() {
+        var currentTheme = document.documentElement.getAttribute("data-theme");
+        var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+        document.documentElement.setAttribute("data-theme", switchToTheme);
+        localStorage.setItem('theme', switchToTheme);
+    }
+});
